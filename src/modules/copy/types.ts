afca8a58d6e7c0b280481b75cfec0ba227e8c040
Yan Tang <360383464@qq.com>
2026-03-05 22:17:53 +0800

    feat: add copy domain types and unit test harness

---
+++ src/modules/copy/types.ts
@@ -0,0 +1,32 @@
+export const CLIPBOARD_FORMATS = [
+  "file-object",
+  "uri-list",
+  "path-text",
+  "none",
+] as const;
+
+export const RESOLVE_ERRORS = [
+  "RESOLVE_EMPTY",
+  "RESOLVE_PARTIAL",
+  "CLIPBOARD_FILE_FAILED",
+  "CLIPBOARD_FALLBACK_USED",
+  "CLIPBOARD_ALL_FAILED",
+] as const;
+
+export type ClipboardFormat = (typeof CLIPBOARD_FORMATS)[number];
+export type ResolveErrorCode = (typeof RESOLVE_ERRORS)[number];
+
+export type MultiPDFMode = "all" | "primary";
+
+export interface ResolvedPDF {
+  itemID: number;
+  attachmentID: number;
+  path: string;
+}
+
+export interface ClipboardResult {
+  ok: boolean;
+  format: ClipboardFormat;
+  count: number;
+  message?: string;
+}
